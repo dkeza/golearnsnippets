301a9c16e56b67a4488937ae987824df38832482
keza <dkezic@yahoo.com>
2018-03-20 20:29:42 +0100

    else if and else

--- ifstatment/main.go
+++ ifstatment/main.go
@@ -4,7 +4,28 @@ import "fmt"
 
 func main() {
 	a, b := 1, 1
+
+	// Simple if
 	if a == b {
 		fmt.Println("Equal")
 	}
+
+	// With else
+	a, b = 1, 2
+	if a == b {
+		fmt.Println("Equal")
+	} else {
+		fmt.Println("Not equal")
+	}
+
+	// With else if
+	a, b = 2, 1
+	if a == b {
+		fmt.Println("Equal")
+	} else if a > b {
+		fmt.Println("a is greater then b")
+	} else {
+		fmt.Println("Not equal")
+	}
+
 }
